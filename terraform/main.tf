1b8ce746c9e34b91c1d5b063f78a052f5581d48d
AoR <96488262+AlfredoOR14@users.noreply.github.com>
2024-02-20 17:25:09 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,11 @@
 provider "aws" {
-  region = "us-east-1"  # Cambia la región según tus preferencias
+    region = "us-east-1"  
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # ID de la AMI de Amazon Linux 2
-  instance_type = "t2.micro"               # Tipo de instancia, puedes ajustarlo según tus necesidades
-
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
   tags = {
-    Name = "MiInstanciaEC2"
+      Name = "TF-Instance"
   }
-}
-
-output "instance_ip" {
-  value = aws_instance.example.public_ip
 }
